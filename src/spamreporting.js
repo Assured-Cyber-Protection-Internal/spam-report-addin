37e58b4eb68a8670909beab6ee1d1c486f0acf93
Matthew Hartley <matthew.hartley@recyber.com>
2024-06-06 14:27:04 +0100

    Report plugin fixes

--- src/spamreporting.js
+++ src/spamreporting.js
@@ -14,10 +14,15 @@ Office.onReady(() => {
   }
 );
   
+
+
+
   // Handles the SpamReporting event to process a reported message.
   function onSpamReport(event) {
-    console.log("onSpamReport");
-    alert("onSpamReport");
+    // body.log("onSpamReport")
+    document.getElementById("ModalFocusTrapZone972").innerHTML += "<h1>HELLO</h1>";
+    showNotification("onSpamReport");
+    // alert("onSpamReport");
     // Get the Base64-encoded EML format of a reported message.
     Office.context.mailbox.item.getAsFileAsync(
       { asyncContext: event },
@@ -30,9 +35,9 @@ Office.onReady(() => {
         }
   
         // Get the user's responses to the options and text box in the preprocessing dialog.
-        const spamReportingEvent = asyncResult.asyncContext;
-        const reportedOptions = spamReportingEvent.options;
-        const additionalInfo = spamReportingEvent.freeText;
+        // const spamReportingEvent = asyncResult.asyncContext;
+        // const reportedOptions = spamReportingEvent.options;
+        // const additionalInfo = spamReportingEvent.freeText;
   
         
         // Run additional processing operations here.
@@ -45,18 +50,55 @@ Office.onReady(() => {
         //   subject: <the subject of the email>
         //   receivedDate: <Date the email was received>
         // }
-        // axios.post('http://127.0.0.1:5000/api/interceptions/report', {
-        //   method: 'POST',
+        var xhr = new XMLHttpRequest();
+        var url = 'https://api-beta.republic.recyber.com/api/interceptions/report';
+
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            var res = JSON.parse(xhr.responseText);
+            console.log(res);
+
+            const event = asyncResult.asyncContext;
+            event.completed({
+              // onErrorDeleteItem: true,
+              // moveItemTo: Office.MailboxEnums.MoveSpamItemTo.JunkFolder,
+              showPostProcessingDialog: {
+                title: "Phishing Reporting",
+                description: "Thank you for reporting this message.",
+              },
+            });
+          }
+        };
+
+        var data = JSON.stringify({
+          from: Office.context.mailbox.item.from.emailAddress,
+          to: Office.context.mailbox.item.to[0].emailAddress,
+          // body: "Axios " + Office.context.mailbox.item.body.getAsync(),
+          body: "Axios",
+          subject: Office.context.mailbox.item.subject,
+          receivedDate: Office.context.mailbox.item.dateTimeCreated
+        });
+
+        xhr.send(data);
+
+        // axios.post('https://api-beta.republic.recyber.com/api/interceptions/report', {
+        //   from: Office.context.mailbox.item.from.emailAddress,
+        //   to: Office.context.mailbox.item.to[0].emailAddress,
+        //   // body: "Axios " + Office.context.mailbox.item.body.getAsync(),
+        //   body: "Axios",
+        //   subject: Office.context.mailbox.item.subject,
+        //   receivedDate: Office.context.mailbox.item.dateTimeCreated
+        // }, {
         //   headers: {
         //     'Content-Type': 'application/json',
-        //   },
-        //   body: JSON.stringify({
-        //     from: Office.context.mailbox.item.from.emailAddress,
-        //     to: Office.context.mailbox.item.to[0].emailAddress,
-        //     body: "Axios " + Office.context.mailbox.item.body.getAsync(),
-        //     subject: Office.context.mailbox.item.subject,
-        //     receivedDate: Office.context.mailbox.item.dateTimeCreated
-        //   })
+        //   }
+        // }).then(res => {
+        //   console.log(res.data)
+        // }).catch(err => {
+        //   console.error(err)
         // })
 // //https://vczw8bvg-5000.uks1.devtunnels.ms/
 //         console.log("Fetching");
@@ -91,15 +133,6 @@ Office.onReady(() => {
          * If an error occurs while the message is being processed,
          * the `onErrorDeleteItem` property determines whether the message will be deleted.
          */
-        const event = asyncResult.asyncContext;
-        event.completed({
-          onErrorDeleteItem: true,
-          moveItemTo: Office.MailboxEnums.MoveSpamItemTo.JunkFolder,
-          showPostProcessingDialog: {
-            title: "Phishing Reporting",
-            description: "Thank you for reporting this message.",
-          },
-        });
       }
     );
   }
